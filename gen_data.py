a02c55e76b408eef9aa09d69290b8dcb8597e6d8
tanmay_kulkarni <tanmaygk97@gmail.com>
2019-04-07 12:18:50 +0530

    finished data_gen

--- gen_data.py
+++ gen_data.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+import pickle
 
 def gen_data(num_maj_class,num_min_class):
 
@@ -9,16 +9,42 @@ def gen_data(num_maj_class,num_min_class):
     cov = [[1,0],[0,1]]
     lower_lim = 0.9
     upper_lim = 2
-
-    x , y = x, y = np.random.multivariate_normal(mean, cov, num_min_class).T
     
 
+
+    x , y  = np.random.multivariate_normal(mean, cov, num_min_class).T
+    data = {}
+    data_points = {}
+    data_points['x'] = x
+    data_points['y'] = y
+    data['minor'] = data_points
+    print("Minority data")
+    print(data)
+
+
+    temp = np.random.uniform(low = lower_lim, high=upper_lim , size = (num_maj_class,2) )
+    x = temp[:,0]
+    y = temp[:,-1]
+    temp = np.random.uniform(low = -upper_lim, high=-lower_lim , size = (num_maj_class,2) )
+    x = np.concatenate((x,temp[:,0]))
+    y = np.concatenate((y,temp[:,1]))
+    
+    data = {}
+    data_points = {}
+    data_points['x'] = x
+    data_points['y'] = y
+    data['major'] = data_points
+    print("Majority data")
+    print(data)
+
+    with open('data.p','wb') as file_handle:
+        pickle.dump(data,file_handle)
 
    
 
 def main():
-    num_maj_class = int("Enter the number of majority class points")
-    num_min_class = int("Enter the number of  minority class points")
+    num_maj_class = int( input ("Enter the number of majority class points") )
+    num_min_class = int( input("Enter the number of  minority class points") )
     gen_data(num_maj_class,num_min_class)
 
 if __name__ == "__main__":
